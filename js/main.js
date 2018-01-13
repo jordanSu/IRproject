bd28aa828dc42acb60fb5b600ee7c19615c92c7c
jordanSu <newjordansu1126@gmail.com>
2018-01-13 18:04:43 +0800

    test for fb post

--- js/main.js
+++ js/main.js
@@ -7,59 +7,56 @@ $('.submit-button').on('click', function(event) {
     category = $(event.target).parent().parent().parent().attr('id');
     content = $(event.target).parent().parent().siblings("textarea").val();
 
-    switch(category) {
-        case "health":
-            url = "http://140.112.106.232:16881/hello";
-            //url = "http://localhost:16881"
-            break;
-        case "politics":
-            url = "http://140.112.106.232:16881/politics";
-            break;
-        case "technology":
-            url = "http://140.112.106.232:16881/technology";
-            break;
-    }
+    //url = "http://140.112.106.232:16881/hello"
+    url = "http://localhost:5000/register";
     console.log('ajax send');
     $.ajax({
         url: url,
         type: "POST",
-        data: "okok",//JSON.stringify({"category": category, "content": content}),
-        contentType: "text/plain"   //"application/json"
-    }).done(function(data){
-            $('#picker').removeAttr('disabled', false);
-            $('#link').attr('disabled', false);
-            $('#post').attr('disabled', false);
-            sentences = data.split("\n");
+        data: JSON.stringify({"category": category, "content": content}),
+        contentType: "application/json"
+    }).success(function(data){
+            $('#picker').attr('disabled', false);
+            $('#picker').data('selectpicker').$button.focus();
+            resp_data = JSON.parse(data);
+            sentences = resp_data.sentences_list;
+
             console.log(sentences);
             for(var i in sentences) {
-                thisone = "<option>" + sentences[i] + "</option>";
+                thisone = "<option>" + sentences[i].trim() + "</option>";
                 $('#picker').append(thisone);
             }
             $('.selectpicker').selectpicker('refresh');
+            $('.pick-button').attr('disabled', false);
+
     });
 });
-
+/*
+$("#picker").on('shown.bs.select', function() {
+    $('div.dropdown-menu').css('overflow', 'scroll');
+})
+*/
 $(".pick-button").on('click', function() {
-    $(this).attr("disabled", true)
-    $("#picker").attr("disabled", true)
+    $(this).attr("disabled", true);
+    $("#picker").attr("disabled", true);
+    $('#link').attr('disabled', false);
+    $('#post').attr('disabled', false);
+    $('.fb-button').attr('disabled', false);
     $('.selectpicker').selectpicker('refresh');
-    selected = $("#picker").find("option:selected")
+    selected = $("#picker").find("option:selected");
 
     for (var i=0; i<selected.length; i++) {
         $("#post").val(function(j, val) {
             return val + selected[i].innerText + "\n";
         });
     }
-})
-
-$(".fb-button").on('click', function(event) {
-    FB.ui({
-        method: 'feed',
-        link: 'https://developers.facebook.com/docs/',
-        caption: 'An example caption',
-    }, function(response){});
 });
 
-$(".new-fb-button").on('click', function(event) {
-    FB.ui()
-})
+$(".fb-button").on('click', function(event) {
+    FB.login(function(){
+        FB.api('/me/feed', 'post', {
+            message: $("#post").val(),
+            link: 'https://developers.facebook.com/docs/'
+        });
+    }, {scope: 'publish_actions'});
+});
